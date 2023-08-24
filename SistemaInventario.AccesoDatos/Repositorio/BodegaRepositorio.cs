c8bf8ccd3178b5d74a71b906d04d98f61d95cca7
Norkch <yesidamaya34@gmail.com>
2023-08-24 16:50:58 -0500

    Se agregan repositorios y servicio para unidad de trabajo.

---
+++ SistemaInventario.AccesoDatos/Repositorio/BodegaRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.AccesoDatos.Repositorio
+{
+    public class BodegaRepositorio : Repositorio<Bodega>, IBodegaRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+        public BodegaRepositorio(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void actualizar(Bodega bodega)
+        {
+            var bodegaBD = _db.Bodegas.FirstOrDefault(b => b.Id == bodega.Id);
+            if (bodegaBD != null)
+            {
+                bodega.Nombre= bodegaBD.Nombre;
+                bodega.Descripcion = bodegaBD.Descripcion;
+                bodega.Estado = bodegaBD.Estado;
+                _db.SaveChanges();
+            }
+        }      
+        
+    }
+}
